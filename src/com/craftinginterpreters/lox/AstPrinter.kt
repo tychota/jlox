6c7074188f8cd9980ad463ed1721243cef4e7d58
tychota <tychota@users.noreply.github.com>
2018-07-14 11:56:19 +0200

    feat: add ast printer

---
+++ src/com/craftinginterpreters/lox/AstPrinter.kt
@@ -0,0 +1,69 @@
+package com.craftinginterpreters.lox
+
+// Creates an unambiguous, if ugly, string representation of AST nodes.
+internal class AstPrinter : Expr.Visitor<String> {
+    override fun visitAssignExpr(expr: Expr.Assign): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visitBinaryExpr(expr: Expr.Binary): String {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right)
+    }
+
+    override fun visitCallExpr(expr: Expr.Call): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visitGetExpr(expr: Expr.Get): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visitGroupingExpr(expr: Expr.Grouping): String {
+        return parenthesize("group", expr.expression);
+    }
+
+    override fun visitLiteralExpr(expr: Expr.Literal): String {
+        return if (expr.value == null) "nil" else expr.value.toString()
+    }
+
+    override fun visitLogicalExpr(expr: Expr.Logical): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visitSetExpr(expr: Expr.Set): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visitSuperExpr(expr: Expr.Super): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visitThisExpr(expr: Expr.This): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visitUnaryExpr(expr: Expr.Unary): String {
+        return parenthesize(expr.operator.lexeme, expr.right)
+    }
+
+    override fun visitVariableExpr(expr: Expr.Variable): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun parenthesize(name: String, vararg exprs: Expr): String {
+        val builder = StringBuilder()
+
+        builder.append("(").append(name)
+        for (expr in exprs) {
+            builder.append(" ")
+            builder.append(expr.accept(this))
+        }
+        builder.append(")")
+
+        return builder.toString()
+    }
+
+    fun print(expr: Expr): String {
+        return expr.accept(this)
+    }
+}
